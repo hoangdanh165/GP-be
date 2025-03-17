c47b6cb1572ff349a453d1757378a9b761367e77
hoangdanh165 <hoangdanh.165@gmail.com>
2025-03-17 16:44:30 +0700

    add default api

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.http import HttpResponse,HttpResponseServerError
+
+def hello_world(request):
+    return HttpResponse("hello world")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_world),
     path('', include('user.urls')),
 ]
