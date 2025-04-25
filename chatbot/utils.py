61cf388b2dac8c93f0c2bd0d9d7412fab52e86e7
hoangdanh165 <hoangdanh.165@gmail.com>
2025-04-26 04:18:10 +0700

    remove supabase

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -1,13 +1,6 @@
-from supabase import create_client
 import os
 from .services.gemini_client_test import get_embedding
 from django.db import connection
-
-
-def get_supabase_client():
-    url = os.getenv("SUPABASE_URL")
-    key = os.getenv("SUPABASE_KEY")
-    return create_client(url, key)
 
 
 def search_similar_services(query, limit=3):
@@ -54,17 +47,3 @@ def search_similar_services(query, limit=3):
 
     print("results: ", results)
     return results
-
-
-# def search_similar_services(query, limit=3):
-#     supabase = get_supabase_client()
-#     query_embedding = get_embedding(query)
-#     result = supabase.rpc(
-#         "match_service_embeddings",
-#         {
-#             "query_embedding": query_embedding,
-#             "match_threshold": 0.8,
-#             "match_count": limit,
-#         },
-#     ).execute()
-#     return result.data
