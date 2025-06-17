890feae8de312b528102545449babeebdef358fe
hoangdanh165 <hoangdanh.165@gmail.com>
2025-06-17 20:27:32 +0700

    update permissions and requirements.txt

--- user/permissions.py
+++ user/permissions.py
@@ -3,23 +3,35 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role.name == "admin"
+        return (
+            hasattr(request.user, "role")
+            and request.user.role.name == "admin"
+            and request.user.status == 1
+        )
 
 
 class IsSale(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role.name == "sale"
+        return (
+            hasattr(request.user, "role")
+            and request.user.role.name == "sale"
+            and request.user.status == 1
+        )
 
 
 class IsCustomer(BasePermission):
     def has_permission(self, request, view):
-        return request.user.role.name == "customer"
+        return (
+            hasattr(request.user, "role")
+            and request.user.role.name == "customer"
+            and request.user.status == 1
+        )
 
 
 class IsAdminOrSale(BasePermission):
-
     def has_permission(self, request, view):
-        return request.user.role.name in [
-            "admin",
-            "sale",
-        ]
+        return (
+            hasattr(request.user, "role")
+            and request.user.role.name in ["admin", "sale"]
+            and request.user.status == 1
+        )
